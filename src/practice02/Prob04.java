66934fe800815b6ed09c8412b6dd1f61de537043
maenttang <36221352+maenttang@users.noreply.github.com>
2018-11-30 16:58:06 +0900

    1차 완료

--- src/practice02/Prob04.java
+++ src/practice02/Prob04.java
@@ -4,30 +4,43 @@ public class Prob04 {
 
 	public static void main(String[] args) {
 		char[] arrayReverse1 = reverse("Hello World");
-		//printCharArray(arrayReverse1);
+		printCharArray(arrayReverse1);
 		
 		char[] arrayReverse2 = reverse("Java Programming!");
-		//printCharArray(arrayReverse2);
+		printCharArray(arrayReverse2);
 	}
 
 	public static char[] reverse(String str) {
-		char[] chars = new char[str.length()]; // chars배열생성
-		int strLengths = str.length(); // 배열길이
+		char[] chars = new char[str.length()];
 		
-		for(int i=0; i<strLengths; i++) { 
+		for(int i=0; i < str.length(); i++) {
+			chars[i] = str.charAt(str.length()-i-1);
+			//System.out.print(chars[i]);
 			
-			chars[i] = str.charAt(i);
-			System.out.print(chars[i]);
 		}
-		
-		for(int i=0; i<strLengths/2; i++) {
-			char temp = chars[i];
-			chars[i] = chars[strLengths-i-1];
-			chars[strLengths-i-1] = temp;
-		
-		}		
+		//System.out.println();
 		return chars;
+		
+		
 	}
+		
+//		char[] chars = new char[str.length()]; // chars배열생성
+//		int strLengths = str.length(); // 배열길이
+//		
+//		for(int i=0; i<strLengths; i++) { 
+//			
+//			chars[i] = str.charAt(i);
+//			System.out.print(chars[i]);
+//		}
+//		
+//		for(int i=0; i<strLengths/2; i++) {
+//			char temp = chars[i];
+//			chars[i] = chars[strLengths-i-1];
+//			chars[strLengths-i-1] = temp;
+//		
+//		}		
+//		return chars;
+	
 	
 	public static void printCharArray(char[] array) {
 		for(int i = 0; i < array.length; i++) {
--- src/practice02/Prob05.java
+++ src/practice02/Prob05.java
@@ -1,8 +1,40 @@
 package practice02;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Prob05 {
 
 	public static void main(String[] args) {
+
+		Random r = new Random();
+		int ran = (int) r.nextInt(100) + 1;
+
+		Scanner scan = new Scanner(System.in);
+
+		while (true) {
+			int num = scan.nextInt();
+			if (num < ran) {
+				System.out.println("더 높게");
+			} else if (num > ran) {
+				System.out.println("더 낮게");
+			} else if (num == ran) {
+				System.out.println("맞았습니다.");
+				System.out.println("다시 하시겠습니까(y/n)");
+				
+				String answer = scan.next();
+				
+				if (answer.equals("y")) {
+					ran = (int) r.nextInt(100) + 1;
+				} else {
+					break;
+				}
+				//break;
+			}
+
+		}
+		scan.close();
+
 	}
 
 }
